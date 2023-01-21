77248e39e67c9c23baf105f6711cc89cb14d03c5
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-01-21 13:16:37 +0600

    Update 1-L5P1_Leetcode-1281.cpp

--- 1-L5P1_Leetcode-1281.cpp
+++ 1-L5P1_Leetcode-1281.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Solution {
+public: 
+  int subtractProductAndSum(int n){
+    int product = 1;
+    int sum = 0;
+    
+    while (n!=0)
+    {
+      int digit = n%10;
+      product = product * digit;
+      sum = sum + digit;
+      
+      n = n/10;
+    }
+    
+    int ans = product-sum;
+    return ans;
+  }
+};
+
+
+//Accepted 
